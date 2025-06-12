9a0c78f8b5953e081a0d6ed39533c861f5e9f9ab
ShaneKirkbride <shane.kirkbride@gmail.com>
2025-06-12 06:35:07 -0600

    Improve URL shortener service

--- UrlCompressorApi/services.py
+++ UrlCompressorApi/services.py
@@ -1,11 +1,14 @@
+"""Utility service implementing Base62 encode/decode."""
+
 import string
 
-# 
+
 class URLShortenerService:
     """
-     Base62 Encoding: Efficiently converts integers to URL-friendly short codes using a combination of letters and digits.
-     Utilizes these methods to generate unique, compact short URLs that redirect to the original long URLs.
+    Base62 Encoding: Efficiently converts integers to URL-friendly short codes using a combination of letters and digits.
+    Utilizes these methods to generate unique, compact short URLs that redirect to the original long URLs.
     """
+
     def __init__(self):
         # Characters used for encoding (Base62)
         self.alphabet = string.ascii_letters + string.digits
@@ -18,10 +21,12 @@ class URLShortenerService:
         """
         s = []
         while num > 0:
-            s.append(self.alphabet[num % self.base])  # Get the character for the remainder
+            s.append(
+                self.alphabet[num % self.base]
+            )  # Get the character for the remainder
             num //= self.base  # Reduce the number for next iteration
         # Reverse the list to get the correct order
-        return ''.join(reversed(s)) or '0'  # Return '0' if num is 0
+        return "".join(reversed(s)) or "0"  # Return '0' if num is 0
 
     def decode(self, short_code: str) -> int:
         """
@@ -30,5 +35,6 @@ class URLShortenerService:
         """
         num = 0
         for char in short_code:
-            num = num * self.base + self.alphabet.index(char)  # Reconstruct the integer ID
+            # ValueError will be raised if an invalid character is supplied
+            num = num * self.base + self.alphabet.index(char)
         return num
